15155b6168eb8b023f4950ec1e519fe25d5eee84
Nkcrossi <crossinicolas@gmail.com>
2024-05-31 14:20:08 -0300

    criar o crud de clientes, profissionais, serviços

---
+++ src/http/controllers/clientes.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { prisma } from "../../../database/prisma/prisma";
+import { hash } from 'bcryptjs'
+import { BarbershopPresenter } from "../../presenters/barbershop-presenter";
+import { z } from "zod";
+
+const clientesBarbershopSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  contactName: z.string(),
+  contactPhone: z.string()
+})
+
+export const clientesBarbershop = async (req: Request, res: Response) => {
+
+  const {name, email, password, contactName, contactPhone} = clientesBarbershopSchema.parse(req.body)
+
+
+  const passwordHash = await hash(password, 6)
+
+  const barbershop = await prisma.barbershop.create({
+    data: {
+      email,
+      password_hash: passwordHash, 
+      name,
+      contact_name: contactName,
+      contact_phone: contactPhone
+    }
+  })
+
+  return res.json({
+    barbershop: BarbershopPresenter.toHttp(barbershop)
+  })
+}
