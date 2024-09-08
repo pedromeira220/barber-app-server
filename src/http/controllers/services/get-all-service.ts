f1e15a1fe797d89cf3edc45d99c45ccba258dc42
Pedro Meira <pedrohmeira@icloud.com>
2024-09-08 15:03:15 -0300

    fix: consertar a rota de buscar serviços

--- src/http/controllers/services/get-all-service.ts
+++ src/http/controllers/services/get-all-service.ts
@@ -1,12 +1,19 @@
 import { Request, Response } from "express";
 import { prisma } from "../../../database/prisma/prisma";
 import { BarbershopPresenter } from "../../presenters/barbershop-presenter";
+import { getBarbershopIdFromJWT } from "../../provider/get-barbershop-id-from-jwt";
 
 export const getAllService = async (req: Request, res: Response) => {
 
-  const service = await prisma.service.findMany()
+  const {id: barbershopIdFromJwt} = getBarbershopIdFromJWT(req)
+
+  const services = await prisma.service.findMany({
+    where: {
+      barbershopId: barbershopIdFromJwt
+    }
+  })
 
   return res.json({
-    barbershops: service
+    services
   })
 }
